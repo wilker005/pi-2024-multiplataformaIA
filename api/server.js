758a732c1b021ec34a5b99841e8030087326dca9
prof Verissimo <carlos.pereira70@fatec.sp.gov.br>
2024-11-18 19:16:00 -0300

    Inclusão do login

--- api/server.js
+++ api/server.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const app = express()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const uniqueValidator = require('mongoose-unique-validator')
 
 app.use(express.json())
 app.use(cors())
@@ -50,6 +53,13 @@ const Evento = mongoose.model('Evento', mongoose.Schema({
     categorias : [Categorias]
 }));
 
+const usuarioSchema = mongoose.Schema({
+    login: {type:String, required: true, unique:true},
+    password: {type: String, required: true}
+})
+usuarioSchema.plugin(uniqueValidator)
+const Usuario = mongoose.model("Usuario", usuarioSchema)
+
 app.get("/eventos", async (req, res) => {
     const eventos = await EventoBase.find()
     res.json(eventos)
@@ -68,6 +78,49 @@ app.post("/eventos", async (req, res) => {
     const eventos = await EventoBase.find()
     res.json(eventos)
 })
+
+app.post('/signup', async(req,res)=> {
+    try{
+        const login = req.body.login
+        const password = req.body.password
+        const cryptografada = await bcrypt.hash(password,10)
+        const usuario = new Usuario({
+            login: login,
+            password: cryptografada
+        })
+        const respostaMongo = await usuario.save()
+        console.log(respostaMongo)
+        res.end()
+    } catch (error) {
+        console.log(error)
+        res.status(409).send("Erro")
+    }
+})
+
+
+app.post('/login', async(req,res)=> {
+    try{
+        const login = req.body.login
+        const password = req.body.password
+        const u = await Usuario.findOne({login:req.body.login})
+        if (!u) {
+            return res.status(401).json({mensagem:"Login inválido"})
+        } 
+        const senhaValida = await bcrypt.compare(password, u.password)
+        if (!senhaValida){
+            return res.status(401).json({mensagem:"Senha inválida"})
+        }
+        const token = jwt.sign(
+            {login: login},
+            "chave-secreta",
+            {expiresIn: "1h"}
+        )
+        res.status(200).json({token:token})
+    } catch (error) {
+        
+    }
+})
+
 
 async function conectarAoMongo() {
     await mongoose.connect(uri, {
--- web/publico/js/frontend.js
+++ web/publico/js/frontend.js
@@ -7,7 +7,25 @@ const fazerLogin = async () => {
     let usuarioLogin = usuarioLoginInput.value
     let passwordLogin = passwordLoginInput.value
     if (usuarioLogin && passwordLogin) {
-        //já já fazemos isso
+        try {
+            const loginEndpoint = '/login'
+            const URLCompleta = `${protocolo}${baseURL}${loginEndpoint}`
+            const response = await axios.post(
+                URLCompleta,
+                {login: usuarioLogin, password: passwordLogin}
+            )
+            usuarioLoginInput.value=""
+            passwordLoginInput.value=""
+            exibirAlerta('.alert-modal-login', "Usuário logado com sucesso!",
+                ['show', 'alert-success'], ['d-none', 'alert-danger'], 2000)
+                ocultarModal('#modalLogin', 2000)
+            const loginLink = document.querySelector('#loginLink')
+            loginLink.innerHTML="Logout"
+
+        }catch (error) {
+            exibirAlerta('.alert-modal-login', 'Falha no login', ['show','alert-danger'], ['d-none', 'alert-success'], 2000)
+            console.log(error)
+        }
     } else{
         exibirAlerta('.alert-modal-login', 'Preencha todos os campos', ['show','alert-danger'], ['d-none', 'alert-success'], 2000)
     }
